db74f54cca6aa7f0984708a2e74411e04bf55b66
Michele <michele@blinkist.com>
2020-02-06 14:41:38 +0100

    removed a line ex12

--- ex12.rb
+++ ex12.rb
@@ -4,7 +4,6 @@ open ("http://www.ruby-lang.org/en") do |f|
   f.each_line { |line| p line}
   puts f.base_uri
   puts f.content_type
-  puts f.charset
   puts f.content_encoding
   puts f.last_modified
-end 
+end
